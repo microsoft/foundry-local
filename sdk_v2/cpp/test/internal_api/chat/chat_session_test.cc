1f8c38870857805a4bb0ef1b0d6d860b090a3112
Prathik Rao <prathikrao@microsoft.com>
2026-09-23 11:04:27 -0700

    cancellation bug fix

--- sdk_v2/cpp/test/internal_api/chat/chat_session_test.cc
+++ sdk_v2/cpp/test/internal_api/chat/chat_session_test.cc
@@ -4021,10 +4021,12 @@ TEST_F(ChatSessionTest, ChatTemplateKwargsCancellationReplaysFullHistory) {
           return cancel_enabled && streamed_items >= 1 ? 1 : 0;
         });
 
-    auto [canceled, canceled_finish] =
-        run_turn(session, {{FOUNDRY_LOCAL_ROLE_USER, kSecondPrompt}}, next_kwargs);
-    (void)canceled;
-    EXPECT_EQ(canceled_finish, FOUNDRY_LOCAL_FINISH_NONE);
+    try {
+      run_turn(session, {{FOUNDRY_LOCAL_ROLE_USER, kSecondPrompt}}, next_kwargs);
+      FAIL() << "Expected streaming callback cancellation";
+    } catch (const fl::Exception& error) {
+      EXPECT_EQ(error.code(), FOUNDRY_LOCAL_ERROR_OPERATION_CANCELLED);
+    }
     EXPECT_EQ(session.TurnCount(), 1u);
     EXPECT_EQ(session.MessageCount(), 2u);
 
